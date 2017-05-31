e267548deb7df8d217258be9980db0ebb9074ed3
saikuso <saikuso@gmail.com>
2017-05-30 18:10:21 -0700

    Add files via upload

--- Ace Buenavides_CIS5_18MAY2017_Array Searching Sorting Program [v.2.0].cpp
+++ Ace Buenavides_CIS5_18MAY2017_Array Searching Sorting Program [v.2.0].cpp
@@ -10,19 +10,18 @@
 #include <ctime>
 #include <limits>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 // Function Prototypes
 int getInteger(string prompt); // Kitty Cat Proof
-void displayList(int fill_random[], const int SZ); // Output Array Values
-void sort(int fill_random[], const int SZ); // Heap Sort
-int binarySearch(int fill_random[], const int SZ, int searchtarget); // Binary Search
+void displayList(vector <int> fill_random); // Output Vector Values
+void sort(vector <int> fill_random, int SZ); // Bubble Sort
+int binarySearch(vector <int> fill_random, int SZ, int searchtarget); // Binary Search
 
 // Variable Tracker
 /*
-const int SZ 			// Array Size Modifier
-int fill_random[SZ] 	// Array variable containing numbers
 int maxrange 			// User-defined max randomly generated number range
 int searchtarget 		// Holds user selected search value
 int searchresult		// Holds the result of the binary search function
@@ -34,16 +33,16 @@ int main ()
 {
 	// Opening Statement
 	cout << "============================================\n";
-	cout << "ARRAY SEARCHING/SORTING PROGRAM [v.2.0]\n";
+	cout << "VECTOR SEARCHING/SORTING PROGRAM [v.2.0]\n";
 	cout << "============================================\n\n";
 	
-	cout << "Welcome to this array searching/sorting program.\n";
-	cout << "This will ask you to input the maximum size for the array\n";
-	cout << "and will randomly generate numbers to fill the array.\n";
-	cout << "The program will output the array of random integers.\n";
-	cout << "It will then sort the array of random integers from\n";	
+	cout << "Welcome to this vector searching/sorting program.\n";
+	cout << "This will ask you to input the maximum size for the vector\n";
+	cout << "and will randomly generate numbers to fill the vector.\n";
+	cout << "The program will output the vector of random integers.\n";
+	cout << "It will then sort the vector of random integers from\n";	
 	cout << "largest to smallest using Bubble Sort. It will then\n";
-	cout << "output the sorted array and prompt you to enter a value\n";
+	cout << "output the sorted vector and prompt you to enter a value\n";
 	cout << "to be searched using Binary Search, and output if it was\n";
 	cout << "found and output the index of where the value was located.\n";
 	cout << "\n\n";
@@ -51,13 +50,17 @@ int main ()
 	cout << "============================================\n\n";
 
 	// Variable Declarations
-	const int SZ = 50;
+	int SZ;
 	int maxrange;
 	int searchtarget;
 	int searchresult;
 	
-	// Declare array 'fill_random' of size SZ
-	int fill_random[SZ];
+	// Prompt the user for the number of elements in the vector
+	SZ = getInteger("Please enter the number of elements in the vector: ");
+	cout << "You entered " << SZ << " as the number of elements in the vector\n" << endl;	
+	
+	// Declare vector "fill_random' of size SZ
+	vector<int> fill_random(SZ);
 	
 	// Prompt for the max range for the numbers to be generated
 	maxrange = getInteger("Please enter the maximum integer that will be randomly generated: ");
@@ -66,7 +69,7 @@ int main ()
 	// Random Number Generator
 	srand ( time(0) + SZ ); // Seeds the random number generator
 	
-	// Fill array 'fill_random' with SZ random numbers and user declared max
+	// Fill vector 'fill_random' with user declared max and SZ
 	for (int i=0; i< SZ; i++)
 	{
 		fill_random[i]=(rand()% maxrange)+1;
@@ -87,7 +90,7 @@ int main ()
 	displayList(fill_random, SZ);
 	
 	// Prompt for user's search target
-	searchtarget = getInteger("Please enter a value to search within the array: ");
+	searchtarget = getInteger("Please enter a value to search within the vector: ");
 	cout << endl;
 	cout << "You entered " << searchtarget << " as your designated search target\n" << endl;
 	
@@ -98,9 +101,9 @@ int main ()
 	// Output Binary Search Results
 	searchresult = binarySearch(fill_random, SZ, searchtarget);
 	if ( searchresult != -1)
-		cout << "Your search target " << searchtarget << " was found at index number " << searchresult << " within the array." << endl;
+		cout << "Your search target " << searchtarget << " was found at index number " << searchresult << " within the vector." << endl;
 	else
-		cout << "Your search target " << searchtarget << " was not found within the array." << endl;
+		cout << "Your search target " << searchtarget << " was not found within the vector." << endl;
 	
 	return 0;
 } // End of Main Function
@@ -135,9 +138,9 @@ int getInteger(string prompt)
 
 
 // displayList (Output) Function (DONE)
-void displayList (int fill_random[], const int SZ)
+void displayList (vector <int> fill_random, int SZ)
 {
-	for (int i=0; i<SZ; i++)
+	for (int i=0; i< fill_random.size()-1; i++)
 	{
 		cout << fill_random[i] << ", ";
 		if ((i + 1) % 10 == 0)
@@ -150,7 +153,7 @@ void displayList (int fill_random[], const int SZ)
 
 
 // Bubble Sort Function (DONE)
-void sort (int fill_random[], const int SZ)
+void sort (vector <int> fill_random, int SZ)
 {
 	bool swapped = false;
 	int holder;
@@ -158,7 +161,7 @@ void sort (int fill_random[], const int SZ)
 	do
 	{
 		swapped = false;
-		for (int i=0; i < (SZ-1); i++)
+		for (int i=0; i < fill_random.size()-1; i++)
 		{
 			if (fill_random[i] < fill_random[i+1])
 			{
@@ -181,7 +184,7 @@ void swap(int &a, int &b)
 } // end of swap function
 
 // Binary Search Function (Elements have to be sorted) (DONE)
-int binarySearch(int fill_random[], const int SZ, int searchtarget)
+int binarySearch(vector <int> fill_random, int SZ, int searchtarget)
 {
 	int first = 0,						// First array element
 		last = SZ - 1,					// Last array element
